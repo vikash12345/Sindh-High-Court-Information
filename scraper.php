a2e7dfbc9e3955bbf7f71e436ff654d9ede4be9a
vikash12345 <vikash@backgroundcheck.email>
2016-08-15 11:48:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,13 +4,22 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
-$MAX_ID = 3; //set based on required maximum numbers
-/** looping over list of ids of doctors **/
-for($id = 1; $i <= $MAX_ID; $id++)
-{
-  // // Read in a MCI doctor page
- $html = scraperwiki::scrape("http://202.61.43.53/cfms-hc-search/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=1&CasesSearch%5BPARTY%5D=&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BisPending%5D=3&page=".$id);    
+// // Read in a page
+$html = scraperwiki::scrape("http://202.61.43.53/cfms-hc-search/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=1&CasesSearch%5BPARTY%5D=&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BisPending%5D=3&page=1");
+//
+// // Find something on the page using css selectors
  $dom = new simple_html_dom();
  $dom->load($html);
- print_r($dom);
-}
+ print_r($dom->find("table.list"));
+//
+// // Write out to the sqlite database using scraperwiki library
+// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+//
+// // An arbitrary query against the database
+// scraperwiki::select("* from data where 'name'='peter'")
+// You don't have to do things with the ScraperWiki library.
+// You can use whatever libraries you want: https://morph.io/documentation/php
+// All that matters is that your final data is written to an SQLite database
+// called "data.sqlite" in the current working directory which has at least a table
+// called "data".
+?>
